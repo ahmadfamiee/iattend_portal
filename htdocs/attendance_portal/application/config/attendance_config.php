d3ad371e20f3c7bd6b6fff038ff056d1f1bfe1dd
Ahmad Fahmi Ghani <135211620+ahmadfamiee@users.noreply.github.com>
2024-11-12 14:50:38 +0800

    Add files via upload

---
+++ htdocs/attendance_portal/application/config/attendance_config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'start_time' => '08:00:00',  // Start of on-time range
+    'late_time' => '09:20:00',   // End of on-time range, after which status becomes "Late"
+    'working_days' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+];
